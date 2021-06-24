049a9d9a67edba1575389c729e071164eab1c59f
Ayush Kumar <ayush.kmr930@gmail.com>
2021-06-24 19:12:17 +0530

    Deleted DFS from this project

--- dfs.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char** argv){
-    cout<<"\nSolving using DFS algorithm";
-    cout<<"\nDFS Not Implmented yet.\nFilename passed: "<<argv[1]<<endl;
-    return 0;
-}
--- launcher.cmd
+++ launcher.cmd
@@ -8,17 +8,6 @@ py displayImage.py maze/%1
 ECHO Running algorithms to find the path to the Treasure ...
 
 
-IF EXIST dfs.cpp (
-    g++ -o dfs dfs.cpp
-    dfs maze/%1
-    IF EXIST mazeSolution/dfs/%1 (
-        py displayImage.py mazeSolution/dfs/%1 DFS
-        GOTO :bfs
-    )
-    ECHO WARNING!!! Solution File not found
-    ECHO HINT: "Make sure to save the solution matrix of maze puzzle at location 'mazeSolution/dfs/' with the same name (in this case '%1') in the C++ program"
-)
-
 :bfs
 IF EXIST bfs.cpp (
     g++ -o bfs bfs.cpp
